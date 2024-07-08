c05d78aaab480de75006520367c1060adbed9c93
Shivani Gupta <shivani.gupta.28.2@mountblue.tech>
2024-07-08 18:34:36 +0530

    Changes in env

---
+++ server/database.js
@@ -0,0 +1,17 @@
+// database.js
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    logging: console.log, // Enable logging for debugging
+  }
+);
+
+module.exports = sequelize;
